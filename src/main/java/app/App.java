9a1fb91039c499428da87df56028fb957f6a22a6
Sarah-C-F <Sarah_Farrens@ymail.com>
2022-08-20 09:48:41 -0700

    added getallmeetings handler and updated app

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,13 +1,19 @@
 package app;
 
 import daos.complaint.ComplaintDaoPostgres;
+import daos.meetings.MeetingDao;
+import daos.meetings.MeetingDaoPostgres;
 import handlers.complaints.CreateComplaintHandler;
+import handlers.meetings.GetAllMeetingsHandler;
 import io.javalin.Javalin;
 import services.complaints.ComplaintServices;
 import services.complaints.ComplaintServicesImpl;
+import services.meetings.MeetingServices;
+import services.meetings.MeetingServicesImpl;
 
 public class App {
     public static ComplaintServices complaintService = new ComplaintServicesImpl(new ComplaintDaoPostgres());
+    public static MeetingServices meetingService = new MeetingServicesImpl(new MeetingDaoPostgres());
 
     public static void main(String[] args){
         //add Javalin
@@ -20,8 +26,14 @@ public class App {
         //Complaint Handlers
         CreateComplaintHandler createComplaintHandler = new CreateComplaintHandler();
 
+        //Meeting Handlers
+        GetAllMeetingsHandler getAllMeetingsHandler = new GetAllMeetingsHandler();
+
         //Call Complaint Handlers
         app.post("/complaints", createComplaintHandler);
+
+        //Call Meeting Handlers
+        app.get("/meetings", getAllMeetingsHandler);
 
         //start app
         app.start();
---
+++ src/main/java/handlers/meetings/GetAllMeetingsHandler.java
@@ -0,0 +1,17 @@
+package handlers.meetings;
+
+import app.App;
+import com.google.gson.Gson;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+public class GetAllMeetingsHandler implements Handler {
+
+    @Override
+    public void handle(@NotNull Context ctx) throws Exception {
+        Gson gson = new Gson();
+        String json = gson.toJson(App.meetingService.getAllMeetings());
+        ctx.result(json);
+    }
+}
