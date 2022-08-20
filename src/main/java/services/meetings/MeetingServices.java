a2d8621c86b767abac8d399773c61fe4e65028cc
Sarah-C-F <Sarah_Farrens@ymail.com>
2022-08-20 09:28:10 -0700

    added meetings Services

---
+++ src/main/java/services/meetings/MeetingServices.java
@@ -0,0 +1,10 @@
+package services.meetings;
+
+import entities.Meeting;
+
+import java.util.ArrayList;
+
+public interface MeetingServices {
+
+    ArrayList<Meeting> getAllMeetings();
+}
---
+++ src/main/java/services/meetings/MeetingServicesImpl.java
@@ -0,0 +1,20 @@
+package services.meetings;
+
+import daos.meetings.MeetingDao;
+import entities.Meeting;
+
+import java.util.ArrayList;
+
+public class MeetingServicesImpl implements MeetingServices{
+
+    private MeetingDao meetingDao;
+
+    public MeetingServicesImpl(MeetingDao meetingDao) {
+        this.meetingDao = meetingDao;
+    }
+
+    @Override
+    public ArrayList<Meeting> getAllMeetings() {
+        return this.meetingDao.getAllMeetings();
+    }
+}
